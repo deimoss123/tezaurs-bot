1ada1c1471ccd8b85dc5b84aa997e9e8cacd6efb
deimoss123 <danielsalksnis@gmail.com>
2022-11-23 23:47:47 +0200

    error check

--- src/utils/findWord.ts
+++ src/utils/findWord.ts
@@ -1,35 +1,44 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import * as cheerio from 'cheerio';
 
-interface FindWordReturn {
+export interface FindWordReturn {
   finalWord: string;
   resultInfoText: string;
 }
 
 export default async function findWord(word: string): Promise<FindWordReturn | void> {
   const url = `https://tezaurs.lv/api/searchEntry?w=${encodeURIComponent(word)}`;
-  
-  const searchResult = await axios.get(url);
+
+  let searchResult: AxiosResponse<any, any>;
+
+  try {
+    searchResult = await axios.get(url);
+  } catch (e) {
+    console.error(e);
+    throw new Error();
+  }
+
   const $ = cheerio.load(searchResult.data);
   const docDiv = $('#doc');
-  
+
   let linkToWord: cheerio.Cheerio | undefined;
   let resultInfoText = 'Atrasta tiešā forma';
-  
+
   // meklē vārdu tieši
   if ($(docDiv).find('#exactMatch').length) {
     linkToWord = $(docDiv).find('#exactMatch > div:nth-child(1) > a');
-  } else { // meklē vārdu netieši
+  } else {
+    // meklē vārdu netieši
     const searchTypes: {
-      id: string, // HTML elementa id
-      text: string // informatīvais teksts, kas tiks parādīts atbildē, piem., Atrasta pamatforma vārdam <vārds>
+      id: string; // HTML elementa id
+      text: string; // informatīvais teksts, kas tiks parādīts atbildē, piem., Atrasta pamatforma vārdam <vārds>
     }[] = [
       { id: '#lemmas', text: 'pamatforma' },
       { id: '#fuzzy', text: 'aptuvena līdzība' },
       { id: '#alternatives', text: 'alternatīva' },
       { id: '#homonyms', text: 'homonīms' },
     ];
-    
+
     for (const { id, text } of searchTypes) {
       const searched = $(docDiv).find(`${id} .listBox > div:nth-child(1) > a`);
       if (searched.length) {
@@ -39,9 +48,9 @@ export default async function findWord(word: string): Promise<FindWordReturn | v
       }
     }
   }
-  
+
   if (!linkToWord || !linkToWord.attr('href')) return;
-  
+
   return {
     finalWord: linkToWord.text(),
     resultInfoText,
