7d25068de406d0b15994127e603a51d2282d19f1
deimoss123 <danielsalksnis@gmail.com>
2023-02-23 17:13:17 +0200

    sveika pasaule (rūsā)

--- src/interfaces/Command.ts
+++
@@ -1,8 +0,0 @@
-import { ApplicationCommandData, CommandInteraction } from 'discord.js';
-
-interface Command {
-  data: ApplicationCommandData
-  run: (i: CommandInteraction) => void
-}
-
-export default Command
--- src/utils/commandList.ts
+++
@@ -1,8 +0,0 @@
-import Command from '../interfaces/Command';
-import tezaurs from '../commands/tezaurs';
-
-const commandList: Command[] = [
-  tezaurs,
-];
-
-export default commandList;
