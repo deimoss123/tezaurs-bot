a8d5809760cc08e52a0d410af48c35821869dc62
daniels <danielsalksnis@gmail.com>
2023-02-25 00:40:22 +0200

    vārdu meklēšana

--- src/commands/tezaurs.rs
+++ src/commands/tezaurs.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use reqwest;
 use scraper::{Html, Selector};
 use serenity::{
@@ -12,27 +13,52 @@ use serenity::{
     prelude::Context,
 };
 
-fn get_text(res: &str) {
-        let fragment = Html::parse_fragment(&res);
-        let div_selector = Selector::parse("#doc").unwrap();
+// -> (link, formatted return string)
+fn find_word(html_string: &str, search_query: &str) -> Option<(String, String)> {
+    // (css class, formatted return string)
+    let search_result_types: Vec<(&str, &str)> = vec![
+        ("#exactMatch", "Vārds <> atrasts tiešā formā"),
+        ("#lemmas", "Atrasta vārda <> pamatforma"),
+        ("#fuzzy", "Atrasta aptuvena līdzība vārdam <>"),
+        ("#alternatives", "Atrasta vārda <> alternatīva"),
+        ("#homonyms", "Atrasts vārda <> homonīms"),
+    ];
 
-        // TODO: ja nav tad tezaura kļūme gan jau
-        let doc_div = fragment.select(&div_selector).next().unwrap();
+    let fragment = Html::parse_fragment(&html_string);
 
-        let exact_match_selector = Selector::parse("#exactMatch").unwrap();
+    for (selector_str, return_str) in search_result_types {
+        let selector = Selector::parse(&selector_str).unwrap();
 
-        let temp = doc_div.select(&exact_match_selector).next();
-        println!("{:#?}", temp);
+        if let Some(frag) = fragment.select(&selector).next() {
+            let a_selector = Selector::parse("a").unwrap();
+            let res = frag.select(&a_selector).next();
+            if let Some(frag1) = res {
+                if let Some(link) = frag1.value().attr("href") {
+                    return Some((
+                        link.to_string(),
+                        Regex::new("<>")
+                            .unwrap()
+                            .replace(return_str, format!("**{}**", search_query))
+                            .to_string(),
+                    ));
+                }
+            }
+            println!("a selector");
+            println!("{:#?}", res);
+        }
+    }
+
+    return None;
+
+    // println!("{:#?}", temp);
 }
 
 pub async fn run(ctx: &Context, interaction: &ApplicationCommandInteraction) {
     let word_option = &interaction.data.options.get(0);
     let word_option = word_option.unwrap().resolved.as_ref().unwrap();
 
-    let user_option = &interaction.data.options.get(1);
-
-    if let CommandDataOptionValue::String(text) = word_option {
-        let user_mention: String = match user_option {
+    if let CommandDataOptionValue::String(search_query) = word_option {
+        let user_mention = match interaction.data.options.get(1) {
             Some(opt) => match opt.resolved.as_ref().unwrap() {
                 CommandDataOptionValue::User(user, _) => format!("<@{}>\n", user.id),
                 _ => "".to_string(),
@@ -40,10 +66,9 @@ pub async fn run(ctx: &Context, interaction: &ApplicationCommandInteraction) {
             None => "".to_string(),
         };
 
-        let res_text = format!("{}Ievadītais vārds: **{}**", user_mention, text);
-        let url = format!("https://tezaurs.lv/api/searchEntry?w={}", text);
+        let url = format!("https://tezaurs.lv/api/searchEntry?w={}", search_query);
         println!("URL: {}", url);
-        let res = reqwest::get(url).await;
+        let res = reqwest::get(&url).await;
         let res = if let Ok(res) = res {
             if let Ok(res) = res.text().await {
                 res
@@ -55,8 +80,10 @@ pub async fn run(ctx: &Context, interaction: &ApplicationCommandInteraction) {
         };
 
         println!("{}", &res);
-        get_text(&res);
-
+        let res_text = match find_word(&res, search_query) {
+            Some((link, text)) => format!("{}{}\n{}\n{}", user_mention, text, url, link),
+            None => format!("Vārds netika atrasts\n{}", url).to_string(),
+        };
 
         // izaicinājums - uzraksti smuku kodu rūsā (neiespējami)
         if let Err(why) = interaction
