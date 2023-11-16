9d1454e87bb1ca9b609a7731148e3a38466c16a8
deimoss123 <danielsalksnis@gmail.com>
2023-11-16 21:06:46 +0200

    Bota statuss

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,26 @@
-import { Client, GatewayIntentBits } from "discord.js";
-import commandHandler from "./utils/commandHandler";
-import validateEnv from "./utils/validateEnv";
-import pg from "pg";
-import autocompleteHandler from "./utils/autocompleteHandler";
-import chalk from "chalk";
+import { Client, GatewayIntentBits } from 'discord.js';
+import commandHandler from './utils/commandHandler';
+import validateEnv from './utils/validateEnv';
+import pg from 'pg';
+import autocompleteHandler from './utils/autocompleteHandler';
+import chalk from 'chalk';
+import setBotPresence from './utils/setBotPresence';
 
 validateEnv();
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 const dbClient = new pg.Client({ connectionString: process.env.DB_URL });
 
-dbClient.connect().then(() => console.log("Connected to DB"));
+dbClient.connect().then(() => console.log('Connected to DB'));
 
-client.once("ready", (client) => {
-  console.log(`${chalk.yellow(client.user.tag)} logged in`);
+client.once('ready', bot => {
+  console.log(`${chalk.yellow(bot.user.tag)} logged in`);
+
+  setBotPresence(bot);
+  setInterval(() => setBotPresence(bot), 3_600_000);
 });
 
-client.on("interactionCreate", (i) => {
+client.on('interactionCreate', i => {
   if (!dbClient.database) return;
 
   if (i.isChatInputCommand()) commandHandler(i);
---
+++ src/utils/setBotPresence.ts
@@ -0,0 +1,17 @@
+import { ActivityType, Client, PresenceUpdateStatus } from 'discord.js';
+
+export default function setBotPresence(client: Client<true>) {
+  const guildCount = client.guilds.cache.size;
+
+  client.user.setPresence({
+    status: PresenceUpdateStatus.DoNotDisturb,
+    activities: [
+      {
+        state: `/tezaurs | ${guildCount} serveros`,
+        // @ts-ignore
+        type: ActivityType.Custom,
+        name: '-',
+      },
+    ],
+  });
+}
